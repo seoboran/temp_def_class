7959db11c00e1b4c97cdaa979289cdeac7039318
seoboran <qhfks3405@gmail.com>
2020-08-25 11:32:53 +0900

    class2

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
-import calculation as cal
+from mathmatic import plus
+from mathmatic import subtract
+from datapreprocessing import processing
+from datapreprocessing import importdata
 
 a=3
 b=4
 
 def main():
-    print("안녕하세요,main() 입니다.")
-    print("a + b =", cal.subtract(a, b))
-    print("a - b =", cal.plus(a, b))
-    print("a*b=",cal.multiple(a,b))
+    print("~~사칙 연산을 시작합니다~~")
+    print("a - b=", subtract.minus(a,b))
+    print("a + b =", plus.add(a, b))
+    print("~~사칙 연산을 종료합니다~~")
+
+data=importdata.readData()
+processing.process_data(data)
 
 if __name__=="__main__":
-    main()
-
+  main()
---
+++ mathmatic/subtract.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+def minus(a,b):
+    c=a-b
+    return c
+
+
